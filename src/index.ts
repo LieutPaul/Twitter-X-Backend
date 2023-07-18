22340c833053f333020cc51ad1f6a6dbb272df07
LieutPaul <Vikas.Kalyanapuram@iiitb.ac.in>
2023-07-18 18:10:46 +0530

    Added CORS

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,16 @@ import userRouter from "./routes/userRoutes";
 import tweetRouter from "./routes/tweetRoutes";
 import authRouter from "./routes/authRoutes";
 import {authenticateUser} from "./middlewares/authMiddleware";
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
-
+app.use(cors());
 app.use("/users", userRouter);
 app.use("/tweets", authenticateUser, tweetRouter);
 app.use("/auth", authRouter);
 
-app.listen(3000, () => {
-    console.log("Listening on port 3000.");
+app.listen(8080, () => {
+    console.log("Listening on port 8080.");
 });
